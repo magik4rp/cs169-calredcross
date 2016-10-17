050d3f206cf5ea7288e7fa38a7a4da1fb9621b24
Huda Iftekhar <hiftekhar@berkeley.edu>
2016-10-17 08:25:06 +0000

    huda made cucumber tests pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016223352) do
+ActiveRecord::Schema.define(version: 20161017072133) do
 
   create_table "news", force: :cascade do |t|
     t.string   "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20161016223352) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "major"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
