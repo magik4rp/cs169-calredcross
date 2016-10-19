7928138a44d9aa18428681bc41df39c38cb1d635
Frances Thai <notfelineit@berkeley.edu>
2016-10-19 03:12:14 +0000

    Finished with Sign-Up page, moving on to Sign-In

--- app/assets/javascripts/devise/new.js
+++ app/assets/javascripts/devise/new.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $("input[type='text'], input[type='email'], input[type='password']").focus(function() {
-          $(this).prev().css("margin-top", "-8px");
+        var label = $("label[for='"+$(this).attr('id')+"']");
+        label.css("margin-top", "-8px");
     });
 });
     
