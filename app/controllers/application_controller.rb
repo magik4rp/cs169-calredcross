d42d3d606a5e04f9f24ab1d8cf3efd9387e2f7e7
Phoebe Vu <phoebevu91@berkeley.edu>
2016-10-30 23:09:08 +0000

    Profile Page backend part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def after_sign_in_path_for(users)
-    news_index_path #your path
+    profile_index_path #your path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'profile/index'
+  # get 'profile/index'
 
   resources :news
   resources :posts
   devise_for :users
   root to: "home#index"
+  resources :profile
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
