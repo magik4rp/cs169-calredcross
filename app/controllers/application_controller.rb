d1e8990ae3bf30dc9d111359f0e6fe5b62c268e1
Phoebe Vu <phoebevu91@berkeley.edu>
2016-10-31 06:43:56 +0000

    PaperClip Test for Avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,14 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
  
+  before_filter :configure_permitted_parameters, :only => [:create]
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :major, :email, :password) }
+  end
+    
   
   def after_sign_in_path_for(users)
     profile_index_path #your path
