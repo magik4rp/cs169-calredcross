1488136fe5d30da2c20cd4bf67ae71c29faa404c
Phoebe Vu <phoebevu91@berkeley.edu>
2016-10-31 08:18:07 +0000

    Added navigation bar on Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :posts
  # devise_for :users
   root to: "home#index"
-  resources :profile
+  resources :profile, only: [:index]
   devise_for :users, :controllers => { :registrations => "registrations" }
   #devise_for :users, :controllers => { :registrations => "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
