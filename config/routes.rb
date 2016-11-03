66f9758c082efa30b9aaabf3156aac3f91e6a5c9
Frances Thai <notfelineit@berkeley.edu>
2016-11-03 00:01:28 +0000

    Frances: Completed server side + database transaction for name and description changes. Working on profile photo.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # get 'profile/index'
-
+  post 'profile/update', :to => 'profile#update'
   resources :news
   resources :posts
  # devise_for :users
   root to: "home#index"
-  resources :profile, only: [:index]
+  resources :profile, only: [:index, :update]
   devise_for :users, :controllers => { :registrations => "registrations" }
   #devise_for :users, :controllers => { :registrations => "registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
