a9567265c92d83d1594d967c7a63c460cc680dd8
Phoebe Vu <phoebevu91@berkeley.edu>
2016-11-16 02:23:01 +0000

    Minor changes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,7 +3,7 @@ class MembersController < ApplicationController
   def index
     @members = User.getMembers
     @is_officer = false 
-    if current_user.is_officer == 1
+    if current_user and current_user.is_officer == 1
       @is_officer = true
     end
   end
