96d3897c9e99083f12daf837737870d8498dffb3
Bozhao Jin <bozhaojin@berkeley.edu>
2016-11-17 00:25:04 +0000

    added associations between galleries and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114232443) do
+ActiveRecord::Schema.define(version: 20161117002235) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20161114232443) do
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_galleries_on_user_id"
   end
 
   create_table "images", force: :cascade do |t|
