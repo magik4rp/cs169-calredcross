d19ac27a33399116c0b49d4440fdd1a4a2c48a16
Huda Iftekhar <hiftekhar@berkeley.edu>
2016-11-17 07:10:00 +0000

    Fixed SimpleCov Statements Huda

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,4 @@
-require 'coveralls'
-Coveralls.wear_merged! 
-require 'coveralls'
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+require 'simplecov' 
 SimpleCov.start do   
   add_filter 'features/support/selectors.rb'
   add_filter 'features/step_definitions/web_steps.rb' 
