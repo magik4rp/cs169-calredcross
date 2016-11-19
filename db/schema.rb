0a28486490ca47285851f112d05f1c1f8b0c95ac
Bozhao Jin <bozhaojin@berkeley.edu>
2016-11-19 17:12:44 +0000

    tentative fix to heroku migration prob

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113191130) do
+ActiveRecord::Schema.define(version: 20161119171142) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
