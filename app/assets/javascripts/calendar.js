a8f9e576daf9c1f06191c3ffdd8eaa56766cc21d
magik4rp <notfelineit@berkeley.edu>
2016-11-24 20:09:49 -0800

    Frances: Calendar functional, starting on lightbox

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
+    	fixedWeekCount: false,
+		googleCalendarApiKey: 'AIzaSyAN3MLI-jD6mS6425sjj9QcBPWykxvsxZY',
+	    events: {
+	        googleCalendarId: 'americanredcrossatcal@gmail.com'
+	    },
+	    eventClick: function(event, jsEvent, view ) { 
+	    	console.log(event);
+	    	jsEvent.preventDefault();
+	    },
+	    height: 550,
+	    aspectRatio: 1.35,
+	});
 
 });
