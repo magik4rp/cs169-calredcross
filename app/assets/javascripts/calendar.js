6ce6f4067843c7b0be0f441148fcb709191ef564
magik4rp <notfelineit@berkeley.edu>
2016-11-24 21:33:00 -0800

    Frances: Calendar front end all finished, need ajax

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -9,11 +9,21 @@ $(document).ready(function() {
 	        googleCalendarId: 'americanredcrossatcal@gmail.com'
 	    },
 	    eventClick: function(event, jsEvent, view ) { 
-	    	console.log(event);
+	    	console.log(event.start);
+	    	$("#event-lightbox-container").show();
 	    	jsEvent.preventDefault();
+	    	$("#event-start").text(event.start.format("dddd, MMMM Do YYYY, h:mm a"));
+	    	$("#event-title").text(event.title);
+	    	$("#event-end").text(event.end.format("dddd, MMMM Do YYYY, h:mm a"));
+	    	
 	    },
 	    height: 550,
 	    aspectRatio: 1.35,
+	    timeFormat: 'h:mm',
+	    timezone: 'local'
 	});
 
+	$("#close-event-lightbox").click(function() {
+		$("#event-lightbox-container").hide();
+	});
 });
