7130a1b041f1495d3c94583ece852ce90a6fefe5
Huda Iftekhar <hiftekhar@berkeley.edu>
2016-12-01 14:35:00 +0000

    another rspec problem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,7 @@ describe User, :type => :model  do
     
     @user_one = User.create!(:id => 6, :name => "Weird Dance", :email => "hi@gmail.com", :password => "miseryisme", :major => "CS")
     it 'should get the member' do 
-        
-        id_num = User.find_by_email("sampleofficer@gmail.com")
-        id_num2 = id_num.id
+         
         @user_three = User.new(:name => "Full Name")
         @user = User.new(:name => "whatver ", :id => 1)
         @user_three.getMember(6)
