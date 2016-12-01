0a3cb82c4e5e831ee4487b74b06e42ca5fa5b489
Phoebe Vu <phoebevu91@berkeley.edu>
2016-12-01 07:16:14 +0000

    Use Current DateTime

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,17 +9,14 @@ class HomeController < ApplicationController
     else
       @info = Info.first
     end
-    @date = DateTime.now.to_s
-    @url = "https://www.googleapis.com/calendar/v3/calendars/americanredcrossatcal@gmail.com/events?key=AIzaSyAN3MLI-jD6mS6425sjj9QcBPWykxvsxZY&timeMin=2016-10-25T11:00:00-07:00"
-    require 'open-uri'
+    @date = Time.now.utc.strftime('%FT%TZ')
+    @url = "https://www.googleapis.com/calendar/v3/calendars/americanredcrossatcal@gmail.com/events?key=AIzaSyAN3MLI-jD6mS6425sjj9QcBPWykxvsxZY&timeMin=" + @date
     
-   
+    require 'open-uri'
     buffer = open(@url).read
     response = JSON.parse(buffer)
     
     @events = response["items"]
-    # @description = @calendar['summary']
-    
   end
   
   
